9728a1c0477169e103265b91f5b12259eee1ccdf
Emilie Ma <kewbish@gmail.com>
2021-11-30 21:24:59 -0800

    Add AOC2021 Day 1 Pt 1

--- aoc2021/01/d1_1.py
+++ aoc2021/01/d1_1.py
@@ -1,2 +1,12 @@
 with open("input.txt") as x:
     lines = x.readlines()
+    levels = [int(line) for line in lines]
+
+prev = levels[0]
+c = 0
+for l in levels:
+    if l > prev:
+        c += 1
+    prev = l
+
+print(c)

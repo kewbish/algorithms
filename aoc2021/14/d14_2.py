ad9da249504baac66b87cd7aa6719e0a0a0b027b
Emilie Ma <kewbish@gmail.com>
2021-12-14 08:31:14 -0800

    Add AOC2021 Day 14 Pt 2

---
+++ aoc2021/14/d14_2.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+with open("input.txt") as x:
+    polymer, instructions = x.read().split("\n\n")
+    instructions = instructions.splitlines()
+    mapping = {}
+    for ins in instructions:
+        key, value = ins.split(" -> ")
+        mapping[key] = value
+
+pairs = [polymer[i : i + 2] for i in range(len(polymer) - 1)]
+
+pair_counter = defaultdict(int)
+for pair in pairs:
+    pair_counter[pair] += 1
+
+
+def step():
+    global pair_counter
+    new_counter = defaultdict(int)
+    for pair in pair_counter.keys():
+        new_counter[pair[0] + mapping[pair]] += pair_counter[pair]
+        new_counter[mapping[pair] + pair[1]] += pair_counter[pair]
+    pair_counter = new_counter
+
+
+for _ in range(40):
+    step()
+
+character_counter = defaultdict(int)
+for pair in pair_counter:
+    character_counter[pair[0]] += pair_counter[pair]
+character_counter[polymer[-1]] += 1
+print(max(character_counter.values()) - min(character_counter.values()))

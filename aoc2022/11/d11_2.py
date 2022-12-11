9703dd881624ad34fc5d8efb28bc8404c8ba16c1
Emilie Ma <kewbish@gmail.com>
2022-12-10 21:28:35 -0800

    Add AOC2022 Day 11 Pt 2

---
+++ aoc2022/11/d11_2.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+class Monkey:
+    items: List[int] = []
+    operation_string: str = ""
+    divisible_by: int = 1
+    if_true: int = 0
+    if_false: int = 0
+    inspects: int = 0
+
+    def __init__(self, monkey_str: str) -> None:
+        lines = monkey_str.split("\n")[1:]
+        self.items = [int(i) for i in lines[0].strip().replace("Starting items: ", "").split(", ")]
+        self.operation_string = lines[1].strip().replace("Operation: new = ", "")
+        self.divisible_by = int(lines[2].strip().split()[-1])
+        self.if_true = int(lines[3].strip().split()[-1])
+        self.if_false = int(lines[4].strip().split()[-1])
+
+
+with open("input.txt") as x:
+    monkeys = x.read().split("\n\n")
+    monkeys = [Monkey(monkey) for monkey in monkeys]
+
+big_mod = 1
+for monkey in monkeys:
+    if big_mod % monkey.divisible_by != 0:
+        big_mod *= monkey.divisible_by
+
+for round_i in range(10000):
+    for monkey in monkeys:
+        while monkey.items:
+            item = monkey.items.pop(0)
+            monkey.inspects += 1
+            new_i = eval(monkey.operation_string.replace("old", str(item)))
+            new_i %= big_mod
+            if new_i % monkey.divisible_by == 0:
+                monkeys[monkey.if_true].items.append(new_i)
+            else:
+                monkeys[monkey.if_false].items.append(new_i)
+
+monkeys.sort(key=lambda m: m.inspects)
+print(monkeys[-1].inspects * monkeys[-2].inspects)

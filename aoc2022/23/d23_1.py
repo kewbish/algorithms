884c2ed2090761854e98e3583e79d058112b95f1
Emilie Ma <kewbish@gmail.com>
2022-12-23 09:26:18 -0800

    Add AOC2022 Day 23 Pt 1

---
+++ aoc2022/23/d23_1.py
@@ -0,0 +1,65 @@
+from collections import defaultdict
+
+
+with open("input.txt") as x:
+    grove = x.read().splitlines()
+    grove = [[pos for pos in row] for row in grove]
+
+elves = set()
+for r in range(len(grove)):
+    for c in range(len(grove[0])):
+        if grove[r][c] == "#":
+            elves.add((r, c))
+
+
+around_check = [[4, 3, 5], [0, 1, 7], [6, 5, 7], [2, 3, 1]]
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+AROUND = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+for _ in range(10):
+    propositions = defaultdict(list)
+    for elf in elves:
+        around_elves = []
+        for delta_r, delta_c in AROUND:
+            other_elf_r, other_elf_c = elf[0] + delta_r, elf[1] + delta_c
+            around_elves.append((other_elf_r, other_elf_c) in elves)
+        if not any(around_elves):
+            propositions[elf].append(elf)
+            continue
+        if not any(around_elves[i] for i in around_check[0]):
+            propositions[(directions[0][0] + elf[0], directions[0][1] + elf[1])].append(elf)
+        elif not any(around_elves[i] for i in around_check[1]):
+            propositions[(directions[1][0] + elf[0], directions[1][1] + elf[1])].append(elf)
+        elif not any(around_elves[i] for i in around_check[2]):
+            propositions[(directions[2][0] + elf[0], directions[2][1] + elf[1])].append(elf)
+        elif not any(around_elves[i] for i in around_check[3]):
+            propositions[(directions[3][0] + elf[0], directions[3][1] + elf[1])].append(elf)
+        else:
+            propositions[elf].append(elf)
+    new_elves = set()
+    for proposition, elves in propositions.items():
+        if len(elves) > 1:
+            new_elves.update(elves)
+        else:
+            new_elves.add(proposition)
+    first = directions.pop(0)
+    directions.append(first)
+    first = around_check.pop(0)
+    around_check.append(first)
+    elves = new_elves
+
+
+min_r, max_r = 0, 0
+min_c, max_c = 0, 0
+for elf in elves:
+    min_r = min(min_r, elf[0])
+    max_r = max(max_r, elf[0])
+    min_c = min(min_c, elf[1])
+    max_c = max(max_c, elf[1])
+
+empty = 0
+for r in range(min_r, max_r + 1):
+    for c in range(min_c, max_c + 1):
+        if (r, c) not in elves:
+            empty += 1
+
+print(empty)
